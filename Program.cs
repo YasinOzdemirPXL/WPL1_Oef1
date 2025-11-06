55eb94fe7e9edac6bbb17c929430e137cebe7529
Yasin <Yasin.ozdemir@student.pxl.be>
2025-11-06 13:21:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPL1_Oef1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
